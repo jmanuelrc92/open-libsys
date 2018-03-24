9cc9bbfe0c06d6e9d67ad23e58bec33cca81c1d1
Unknown <juan.02324@gmail.com>
2018-03-23 21:26:23 -0600

    Authentication
    Authentication component configuration.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -43,13 +43,27 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
-
         /*
          * Enable the following components for recommended CakePHP security settings.
          * see https://book.cakephp.org/3.0/en/controllers/components/security.html
          */
         //$this->loadComponent('Security');
         //$this->loadComponent('Csrf');
+        $this->loadComponent('Auth', [
+            'authenticate' => [
+                'Form' => [
+                    'fields' => [
+                        'username' => 'username',
+                        'password' => 'password'
+                    ]
+                ]
+            ],
+            'loginAction' => [
+                'controller' => 'users',
+                'action' => 'login'
+            ],
+            'storage' => 'Session'
+        ]);
     }
     
     public function beforeRender(Event $event)
