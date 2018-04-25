0497baecbac2b5d2d70a11cb7156b0af290ef85a
Unknown <juan.02324@gmail.com>
2018-04-25 12:59:56 -0500

    debugkit disabling

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -215,10 +215,11 @@ Type::build('timestamp')
  * Only try to load DebugKit in development mode
  * Debug Kit should not be installed on a production system
  */
+    /*
 if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
 }
-
+*/
 Plugin::load('Migrations');
 Plugin::load('AdminLTE', ['bootstrap' => true, 'routes' => true]);
 
